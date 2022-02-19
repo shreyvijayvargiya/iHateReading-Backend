e32eafa7a63710d51637f7519ecda44dd7347176
shreykoo <shrey.vijayvargiya@kooapp.com>
2022-02-19 13:23:41 +0530

    ready to deploy API

--- controllers/scrap/index.js
+++ controllers/scrap/index.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
 const { z } = require("zod");
+const fs = require('fs');
+const stream = require("stream");
 
 const scrapLink = async(req, res) => {
     const response = {
@@ -13,20 +15,37 @@ const scrapLink = async(req, res) => {
         response.message = 'Please send link';
         res.send(response)
     }else {
-        const browser = await puppeteer.launch({});
-        const page = await browser.newPage();
-        await page.goto(link);
-        const h1 = await page.title();
-        await page.waitForSelector("h1");
-        await page.waitForSelector("h2");
-        const element = await page.$('h2');
-        const h2 = await page.evaluate(element => element.textContent, element);
-        response.data = {
-            h1,
-            h2,
-            link
-        };
-        res.send(response)
+        try {
+            const browser = await puppeteer.launch({});
+            const page = await browser.newPage();
+            await page.goto(link);
+            const title = await page.title();
+            let h1, h2,thumbnail;
+            if(await page.$('h1') !== null){
+                await page.waitForSelector("h1", { timeout: 5000 });
+                h1 = await page.$('h1');
+                h1 = await page.evaluate(element => element.textContent, h1)
+            }
+            if(await page.$('h2') !== null){
+                await page.waitForSelector("h2", { timeout: 5000 });
+                h2 = await page.$('h2');
+                h2 = await page.evaluate(element => element.textContent, h2);
+            }
+            response.data = {
+                title,
+                h1,
+                h2,
+                link
+            };
+            res.send(response)
+        }catch(e){
+            console.log(e, 'e');
+            res.send({
+                data: null,
+                error: true,
+                message: e
+            })
+        }
     }
 };
 
