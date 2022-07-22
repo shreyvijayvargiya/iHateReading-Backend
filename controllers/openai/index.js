0fa3b08d74a6588ea1d9971bbe459576f356bcb2
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2022-07-22 19:27:33 +0530

    openai token and package added

---
+++ controllers/openai/index.js
@@ -0,0 +1,26 @@
+const { Configuration, OpenAIApi } = require("openai");
+const admin = require("firebase-admin");
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_TOKEN
+});
+
+const createSummary = (req, res) => {
+  const { logId } = req.body;
+  try{
+    const openai = new OpenAIApi(configuration);
+    const response = openai.createCompletion({
+			model: "text-davinci-002",
+      prompt: "",
+      temperature: 0.5,
+      max_tokens: 1000
+		});
+
+  }catch(error) {
+    console.log(error, "error")
+    res.send("Error", error);
+  }
+
+};
+
+module.exports = { createSummary }
