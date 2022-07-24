ac871ecc653b922a2cb55dd0258f9a2bfad31cbf
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2022-07-24 23:49:10 +0530

    cms routes from routes folder removed

--- routes/cms/index.js
+++
@@ -1,6 +0,0 @@
-// import express from "express";
-// import { postToDevCommunity } from "../../controllers/cms";
-// const cmsRouter = express.Router();
-// cmsRouter.post("/api/v1/post-to-dev-community", postToDevCommunity);
-
-// export default cmsRouter;
